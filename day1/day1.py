2a36b8a7bc27433cd5f1a4a52f011a1ba509497a
waspthebughunter <100480448+waspthebughunter@users.noreply.github.com>
2022-04-25 12:57:57 +0300

    Create day1.py

---
+++ day1/day1.py
@@ -0,0 +1,49 @@
+from platform import python_version
+
+print("Current Python Version-", python_version())
+
+########################################################
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3 % 4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+#######################################################
+
+my_name = "my name"
+my_family_name = "my family name"
+my_country = "my country"
+message = "I am enjoying 30 days of python"
+
+#########################################################
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type(my_name))
+print(type(my_family_name))
+print(type(my_country))
+
+##########################################################
+
+print(type(1))
+print(type(1.1))
+print(type(1+1j))
+print(type(my_name))
+print(type(True))
+print(type([1, 2, 3, 4, 5]))
+print(type((1, 2, 3, 4, 5)))
+print(type({1, 2, 3, 4, 5}))
+print(type({1: 1}))
+
+###########################################################
+
+print(((2-10)**2 + (3-8)**2)**0.5)
+
+###########################################################
